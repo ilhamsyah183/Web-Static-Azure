23679fc702e2c3033c38cbcec289cabbe0901aff
M ILHAMSYAH <63888235+ilhamsyah183@users.noreply.github.com>
2021-01-18 19:30:19 +0700

    Delete gympedia directory

--- gympedia/konfigurasiDatabase/helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("BASE_URL","https://localhost/gympedia/");
-
-function direct ($url){
-    echo "<script> window.location = '$url'; </script>";
-}
-
-?>
--- gympedia/konfigurasiDatabase/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database ="gympedia";
-
-    $koneksi = mysqli_connect($server, $username, $password, $database) or die("koneksi ke database gagal");
-
-    ?>
