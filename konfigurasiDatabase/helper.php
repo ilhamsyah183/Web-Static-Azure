49d447ff132b7d6e1acad13a83c0888e7592ee56
M ILHAMSYAH <63888235+ilhamsyah183@users.noreply.github.com>
2021-01-18 19:33:56 +0700

    Add files via upload

---
+++ konfigurasiDatabase/helper.php
@@ -0,0 +1,8 @@
+<?php
+define("BASE_URL","https://localhost/gympedia/");
+
+function direct ($url){
+    echo "<script> window.location = '$url'; </script>";
+}
+
+?>
---
+++ konfigurasiDatabase/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database ="gympedia";
+
+    $koneksi = mysqli_connect($server, $username, $password, $database) or die("koneksi ke database gagal");
+
+    ?>
